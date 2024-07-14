395938dcd0eca985863ef010fdc7e03ae1725066
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-14 11:26:55 +0800

    added the first file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Set up your web servers for the deployment of web_static
+
+# Install nginx
+apt update -y
+apt install nginx -y
+
+# Create the file structure
+mkdir /data/web_static/releases/test/ /data/web_static/shared /data/web
+touch /data/web_static/releases/test/index.html
+# Create a symbolic link
+ln -s -f /data/web_static/releases/test/ /data/webstatic/current
+# Change ownership
+chown -R ubuntu:ubuntu /data/
+sed '/server {\n/\tlocation /hbnb_static/ {\n\troot /data/web_static/current/;\n\talias /data/web_static/current/hbnb_static/;\n\t autoindex off;\n}'
+
