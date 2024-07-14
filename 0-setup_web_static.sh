9956132ba3b45bb4efe7524fbc7be0e46b1930d2
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-07-14 12:28:24 +0800

    exit successfully every time

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Set up your web servers for the deployment of web_static
 
+#Exit immediately if a command exits with a non-zero status
+
+set -e
+
 # Install nginx
 apt update -y
 apt install nginx -y
@@ -15,7 +19,15 @@ echo "<html>
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
 # Change ownership
 chown -R ubuntu:ubuntu /data/
-sed -i '/server {/a\
-        location /hbnb_static/ {\n\talias /data/web_static/current/hbnb_static/;\n\t autoindex off;\n}' /etc/nginx/nginx.conf
-service nginx restart
-
+#Update Nginx configuration to serve the content
+if grep -q "location /hbnb_static/" /etc/nginx/nginx.conf; then
+	echo ""
+else 
+	sed -i '/server {/a\
+	        location /hbnb_static/ {\n\talias /data/web_static/current/hbnb_static/;\n\t autoindex off;\n}' /etc/nginx/nginx.conf
+fi
+if nginx -t; then
+	sudo service nginx restart
+else
+	exit
+fi
