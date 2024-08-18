3290273209a06ac411eaccfef926eaaa47fabb06
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-08-18 19:43:26 +0800

    added some style

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -21,16 +21,18 @@ def teardown_storage(exception):
     """Closes storage on teardown"""
     storage.close()
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     dictionary = {}
     states = list(storage.all(State).values())
     states.sort(key=lambda x: x.name)
     cities = list(storage.all(City).values())
-    cities.sort(key=lambda x:x.name)
-    return render_template('8-cities_by_states.html', states=states, cities=cities)
+    cities.sort(key=lambda x: x.name)
+    return render_template(
+        '8-cities_by_states.html',
+        states=states, cities=cities)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
