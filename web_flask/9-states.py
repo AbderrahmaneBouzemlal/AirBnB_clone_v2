68cb59eccf9500beb2e2cd868a60f8f50b3edfb5
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2024-08-19 16:48:30 +0800

    fixed a bug

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -27,19 +27,14 @@ def teardown_storage(exception):
     strict_slashes=False)
 @app.route('/states/<stateId>', strict_slashes=False)
 def states(stateId=None):
-    states = list(storage.all(State).values())
-    cities = list(storage.all(City).values())
-    cities.sort(key=lambda x: x.name)
-    states.sort(key=lambda x: x.name)
+    states = storage.all(State)
 
-    if stateId is not None and stateId in states:
+    if stateId is not None:
         stateId = f"State.{stateId}"
-    elif stateId not in states:
-        abort(404)
 
     return render_template(
         '9-states.html',
-        states=states, cities=cities, stateId=stateId)
+        states=states, stateId=stateId)
 
 
 if __name__ == '__main__':
